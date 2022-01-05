791265c1b1e16bc2e2d06d26aafcdd1180628bbb
vinhtieng <vinhtieng123@gmail.com>
2022-01-05 09:07:42 +0700

    client - server

--- client/src/credentials.js
+++ client/src/credentials.js
@@ -1,3 +1,3 @@
 export const COOKIE_NAME = 'user login to website?'
 export const TOKEN_NAME = "token_user_vt_ecommerce_name"
-export const SERVER_API = "http://localhost:4000/api/"
+export const SERVER_API = "https://nguyen-vinh-tieng-ecommerce.herokuapp.com/api/"
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const express = require('express');
 const app = express()
 const cors = require('cors')
@@ -14,12 +13,12 @@ app.use(cookieParser({ secret: credentials.COOKIE_SECRET }))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 route(app)
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', "build", "index.html"))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, 'client/build')))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', "build", "index.html"))
+//     })
+// }
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`)
 })
